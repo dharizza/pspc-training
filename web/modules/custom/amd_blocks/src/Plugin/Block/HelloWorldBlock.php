b8d35111140dba390288f5a83ed618b9d528f933
Dharizza Espinach <dharizza@gmail.com>
2025-04-17 15:08:24 +0000

    Change discovery mechanism from Doctrine annotation to PHP Attribute.

--- web/modules/custom/amd_blocks/src/Plugin/Block/HelloWorldBlock.php
+++ web/modules/custom/amd_blocks/src/Plugin/Block/HelloWorldBlock.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace Drupal\amd_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Provides an amd hello world block block.
- *
- * @Block(
- *   id = "amd_blocks_hello_world",
- *   admin_label = @Translation("AMD Hello World Block"),
- *   category = @Translation("Custom"),
- * )
  */
+#[Block(
+  id: "amd_blocks_hello_world",
+  admin_label: new TranslatableMarkup("AMD Hello World Block"),
+  category: new TranslatableMarkup("Custom"),
+)]
 final class HelloWorldBlock extends BlockBase {
 
   /**
