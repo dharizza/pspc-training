84b2349e87662588059239871a760ba51a5a8034
Dharizza Espinach <dharizza@gmail.com>
2025-04-23 14:21:01 +0000

    Change the data displayed in the deletion records list page.

--- web/modules/custom/audit/src/DeletionRecordListBuilder.php
+++ web/modules/custom/audit/src/DeletionRecordListBuilder.php
@@ -18,7 +18,6 @@ final class DeletionRecordListBuilder extends EntityListBuilder {
   public function buildHeader(): array {
     $header['id'] = $this->t('ID');
     $header['label'] = $this->t('Label');
-    $header['uid'] = $this->t('Author');
     $header['created'] = $this->t('Created');
     $header['changed'] = $this->t('Updated');
     return $header + parent::buildHeader();
@@ -31,11 +30,6 @@ final class DeletionRecordListBuilder extends EntityListBuilder {
     /** @var \Drupal\audit\DeletionRecordInterface $entity */
     $row['id'] = $entity->id();
     $row['label'] = $entity->toLink();
-    $username_options = [
-      'label' => 'hidden',
-      'settings' => ['link' => $entity->get('uid')->entity->isAuthenticated()],
-    ];
-    $row['uid']['data'] = $entity->get('uid')->view($username_options);
     $row['created']['data'] = $entity->get('created')->view(['label' => 'hidden']);
     $row['changed']['data'] = $entity->get('changed')->view(['label' => 'hidden']);
     return $row + parent::buildRow($entity);
