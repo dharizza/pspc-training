85b820272fa6c501038dfa72255160158a6a3b8e
Dharizza Espinach <dharizza@gmail.com>
2025-04-23 14:22:27 +0000

    Changed description of created and changed fields in deletionRecord entity.

--- web/modules/custom/audit/src/Entity/DeletionRecord.php
+++ web/modules/custom/audit/src/Entity/DeletionRecord.php
@@ -94,7 +94,7 @@ final class DeletionRecord extends ContentEntityBase implements DeletionRecordIn
 
     $fields['created'] = BaseFieldDefinition::create('created')
       ->setLabel(t('Authored on'))
-      ->setDescription(t('The time that the deletion record was created.'))
+      ->setDescription(t('The time that the entity being deleted was created.'))
       ->setDisplayOptions('view', [
         'label' => 'above',
         'type' => 'timestamp',
@@ -109,7 +109,7 @@ final class DeletionRecord extends ContentEntityBase implements DeletionRecordIn
 
     $fields['changed'] = BaseFieldDefinition::create('changed')
       ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the deletion record was last edited.'));
+      ->setDescription(t('The time that the entity being deleted was last edited.'));
 
     $fields['deleted'] = BaseFieldDefinition::create('timestamp')
       ->setLabel(t('Deleted'))
