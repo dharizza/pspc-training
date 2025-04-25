da8cf3f1a7223717c5a4fb61b8431aaf50b6cb1d
Dharizza Espinach <dharizza@gmail.com>
2025-04-25 15:45:51 +0000

    Implement dependency injection on text transformations service.

--- web/modules/custom/amd_blocks/src/TextTransformations.php
+++ web/modules/custom/amd_blocks/src/TextTransformations.php
@@ -4,16 +4,31 @@ declare(strict_types=1);
 
 namespace Drupal\amd_blocks;
 
+use Drupal\Core\Logger\LoggerChannel;
+use Drupal\Core\Logger\LoggerChannelFactory;
+
 /**
  * Service that provides multiple text transformations.
  */
 final class TextTransformations {
 
   /**
+   * Logger channel.
+   * 
+   * @varDrupal\Core\Logger\LoggerChannel
+   */
+  protected LoggerChannel $logger;
+
+  public function __construct(LoggerChannelFactory $loggerFactory) {
+    $this->logger = $loggerFactory->get('amd_channel');
+  }
+
+  /**
    * Reverse the text received. Example: text to txet.
    */
   public function reverse($text): string {
-    \Drupal::logger('amd_channel')->warning('The text was reversed.');
+    // \Drupal::logger('amd_channel')->warning('The text was reversed.');
+    $this->logger->warning('The text was reversed.');
     return strrev($text);
   }
 
@@ -21,7 +36,8 @@ final class TextTransformations {
    * Uppercase all the text received. Example: text to TEXT.
    */
   public function uppercase($text): string {
-    \Drupal::logger('amd_channel')->warning('The text was transformed to be uppercase.');
+    // \Drupal::logger('amd_channel')->warning('The text was transformed to be uppercase.');
+    $this->logger->warning('The text was transformed to be uppercase.');
     return strtoupper($text);
   }
 
@@ -29,7 +45,8 @@ final class TextTransformations {
    * Lowercase all the text received. TEXT to text.
    */
   public function lowercase($text): string {
-    \Drupal::logger('amd_channel')->warning('The text was transformed to be lowercase.');
+    // \Drupal::logger('amd_channel')->warning('The text was transformed to be lowercase.');
+    $this->logger->warning('The text was transformed to be lowercase.');
     return strtolower($text);
   }
 
@@ -37,7 +54,8 @@ final class TextTransformations {
    * Title case all the text received. 'Example Text' to 'Example text'.
    */
   public function titleCase($text): string {
-    \Drupal::logger('amd_channel')->warning('The text was transformed to be titlecase.');
+    // \Drupal::logger('amd_channel')->warning('The text was transformed to be titlecase.');
+    $this->logger->warning('The text was transformed to be titlecase.');
     return ucfirst($text);
   }
 
