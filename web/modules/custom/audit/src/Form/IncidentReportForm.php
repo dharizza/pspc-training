f53ec11071c1d8117f157f90fe7fb7e8bb6eb3bb
Dharizza Espinach <dharizza@gmail.com>
2025-04-25 14:33:44 +0000

    Populate the deletion records in the select field for the incident report form.

--- web/modules/custom/audit/src/Form/IncidentReportForm.php
+++ web/modules/custom/audit/src/Form/IncidentReportForm.php
@@ -42,7 +42,7 @@ final class IncidentReportForm extends FormBase {
       '#type' => 'select',
       '#title' => $this->t('Select the entity that was deleted incorrectly'),
       '#required' => TRUE,
-      '#options' => [],
+      '#options' => $this->getEntities(),
     ];
 
     $form['report'] = [
@@ -60,6 +60,23 @@ final class IncidentReportForm extends FormBase {
     ];
 
     return $form;
+  }
+
+  public function getEntities() {
+    $storage = \Drupal::entityTypeManager()->getStorage('deletion_record');
+    $query = $storage->getQuery();
+    $query->accessCheck(true);
+    $query->sort('deleted', 'DESC');
+    $ids = $query->execute();
+
+    $records = $storage->loadMultiple($ids);
+
+    $options = [];
+    foreach ($records as $key => $item) {
+      $options[$key] = $item->label->value;
+    }
+
+    return $options;
   }
 
   /**
